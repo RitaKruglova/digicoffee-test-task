455bf8602dbcec1627f7b5cc7dcb8a375e0dd030
RitaKruglova <kruglova404@mail.ru>
2024-05-28 21:07:45 +0300

    feat: add marking for profile

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -1,13 +1,27 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import profileStyles from './profile.module.css';
+import ProfileInfo from '../../components/profile-info/profile-info';
+import { useAppDispatch, useAppSelector } from '../../hooks/reduxHooks';
+import { fetchUserById } from '../../store/slices/userSlice';
 
 const Profile: FC = () => {
+  const userInfo = useAppSelector(store => store.user.userInfo);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUserById(Number(localStorage.getItem('userId'))));
+  }, [dispatch]);
+
   return (
     <section className={profileStyles.container}>
       <h2 className={profileStyles.title}>Мой профиль</h2>
       <div className={profileStyles.nameContainer}>
-        
+        <ProfileInfo name="Имя" value={userInfo.first_name} />
+        <ProfileInfo name="Фамилия" value={userInfo.last_name} />
       </div>
+      <ProfileInfo name="Username" value={userInfo.username} />
+      <ProfileInfo name="Email" value={userInfo.email} />
+      {localStorage.getItem('role') === 'admin' && <ProfileInfo name="Роль" value={userInfo.role} />}
     </section>
   )
 }
