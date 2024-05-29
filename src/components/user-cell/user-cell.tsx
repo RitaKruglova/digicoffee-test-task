12d0e63906e8a69160f3a6bed105b929c38356a2
RitaKruglova <kruglova404@mail.ru>
2024-05-29 14:50:48 +0300

    feat: add marking for UserRow component

--- src/components/user-cell/user-cell.tsx
+++ src/components/user-cell/user-cell.tsx
@@ -3,12 +3,13 @@ import userCellStyles from './user-cell.module.css';
 
 interface IUserCellProps {
   text: string;
+  isBold: boolean;
 }
 
-const UserCell: FC<IUserCellProps> = ({ text }) => {
+const UserCell: FC<IUserCellProps> = ({ text, isBold }) => {
   return (
     <div className={userCellStyles.container}>
-      <p className={userCellStyles.text}>{text}</p>
+      <p className={`${userCellStyles.text} ${isBold ? userCellStyles.bold : ''}`}>{text}</p>
     </div>
   )
 }
--- src/components/user-row/user-row.tsx
+++ src/components/user-row/user-row.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import userRowStyles from './user-row.module.css';
+import UserCell from '../user-cell/user-cell';
 
 interface IUserRowProps {
   username: string;
@@ -12,7 +13,11 @@ interface IUserRowProps {
 const UserRow: FC<IUserRowProps> = ({ username, firstName, lastName, email, role }) => {
   return (
     <div className={userRowStyles.container}>
-      
+      <UserCell text={username} isBold={false} />
+      <UserCell text={firstName} isBold />
+      <UserCell text={lastName} isBold />
+      <UserCell text={email} isBold={false} />
+      <UserCell text={role} isBold />
     </div>
   )
 }
